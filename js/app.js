f542710ce0da9367abe15b29102035c89a397685
Tolulope Dada <tdada0912@gmail.com>
2022-05-29 20:57:01 +0100

    code optimization for better readability

--- js/app.js
+++ js/app.js
@@ -13,11 +13,13 @@ document.getElementById("title").style.fontSize = "19px";
 document.getElementById("title").style.color = "#f3b72a";
 document.querySelector(".team-members").style.display = "flex";
 
+console.log("Question 2");
 // Solution to Question 2
 
 const teamMembers = ["Simple_Soul", "TJ", "OreSause", "Luchii", "Khalid", "Tolu", "Etinosa", "MavDavis", "HayJ"];
 console.log(teamMembers[5]);
 
+console.log("Question 3");
 // Solution to Question 3 
 
 const Person = {
@@ -30,8 +32,10 @@ const Person = {
     state: "Lagos",
     groupName: "Zeus"
 }
-    console.log(`My best movies is ${Person["bestMovie"]}`);
 
+console.log(`My best movie is ${Person["bestMovie"]}`);
+
+console.log("Question 4");
 // Solution to Question 4 
 
 const firstName = "Tolu";
@@ -44,14 +48,16 @@ console.log(firstName + " can also " + hobby + " in deep waters");
 console.log("How " + speed + " can you " + hobby + "?");
 console.log("I can " + hobby + " moderately " + speed);
 
+console.log("Question 5");
 // Solution to Question 5
 
 function remainder(num1, num2) {
     return ((num1 % num2));
 }
 
-console.log("The remainder is", remainder(5, 5));
+console.log("The remainder of the division of 100 and 27 is", remainder(100, 27));
 
+console.log("Question 6");
 // Solution to Question 6
 
 function quadraticSolution(a, b, c) {
@@ -64,6 +70,7 @@ function quadraticSolution(a, b, c) {
 
 console.log("The solution of the quadratic equation is", quadraticSolution(1, -6, 8));
 
+console.log("Question 7");
 // Solution to Question 7 
 
 const myNoun = "dog";
@@ -75,6 +82,7 @@ const wordBlanks = "My " + myNoun + " " + myAdverb + " " + myVerb + " after the 
 
 console.log(wordBlanks);
 
+console.log("Question 8");
 // Solution to Question 8
 
 function areaOfCircle(radius) {
@@ -85,6 +93,7 @@ function areaOfCircle(radius) {
 
 console.log("The area of the circle of radius 9 is", (areaOfCircle(9)).toFixed(2));
 
+console.log("Question 9");
 // Solution to Question 9
 
 const principal = 8200;
@@ -95,11 +104,13 @@ const simpleInterest = Math.floor((principal * rate * time) / 100);
 const remaining = (principal * rate * time) % 100;
 console.log(`The Simple Interest obtained is #${simpleInterest}.${remaining}K`);
 
+console.log("Question 10");
 // Solution to Question 10
 
 const result = 10 % 4;
 console.log("The remainder of the division of 10 & 4 is", result);
 
+console.log("Question 11a");
 // Solution to Question 11 (first version)
 
 const meritMass = 78;
@@ -116,6 +127,7 @@ const nutJobBmi = (nutJobMass / (nutJobHeight ** 2)).toFixed(3);
 console.log((meritBmi > nutJobBmi) ? meritHigherBmi +
  ` which is ${meritBmi}` : `NutJob has a higher BMI which is ${nutJobBmi}`);
 
+ console.log("Question 11b");
 // Solution to Question 11 (second version)
 
 function calculateBMI(mass, height) {
